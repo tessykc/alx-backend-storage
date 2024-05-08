524da86db790d5139d10260cb87ddd24fe220d2d
Kelechi Obi <72105538+tessykc@users.noreply.github.com>
2024-05-08 09:21:22 +0100

    Create 0-uniq_users.sql

---
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -0,0 +1,10 @@
+-- A table that creates user data with:
+-- id, integer, never null, auto increment and primary key
+-- email, string (255 characters), never null and unique
+-- name, string (255 characters)
+
+CREATE TABLE IF NOT EXISTS users (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    name VARCHAR(255)
+);
