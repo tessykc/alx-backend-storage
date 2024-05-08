07d6b47c5524e48e6e98dbb5915b87dd306c6c50
Kelechi Obi <72105538+tessykc@users.noreply.github.com>
2024-05-08 10:42:08 +0100

    Create 8-index_my_names.sql

---
+++ 0x00-MySQL_Advanced/8-index_my_names.sql
@@ -0,0 +1,4 @@
+--Write a SQL script that creates an index idx_name_first on the table names and the first letter of name.
+--Import this table dump: names.sql.zip
+--Only the first letter of name must be indexed
+CREATE INDEX idx_name_first ON names (LEFT(name, 1));
